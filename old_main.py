1e2e10530f3cd28f17d5c83b89c0aff03bc84ec9
kinndannnokajitu <xiangyinbencheng@gmail.com>
2025-08-19 02:51:26 +0900

    リアルタイム音声取得の際、合言葉検知と悲鳴検知とでCHUNK_SECONDSが異なるため、その処理を反映させてほしい意味を込めて一部コメントアウトから解除

--- old_main.py
+++ old_main.py
@@ -32,63 +32,63 @@
 
 #     print("初期化完了")
 
-#     # リアルタイム音声処理の事前情報の定義
-#     SCREAM_CHUNK_SECONDS = 3
-#     KEYWORD_CHUNK_SECONDS = 5
-#     RATE = 16000
-#     CHANNELS = 1
-#     FORMAT = pyaudio.paInt16
-#     SCREAM_CHUNK_SIZE = int(RATE * SCREAM_CHUNK_SECONDS)
-#     # 合言葉検知用に過去五秒間の音声データを保持するためのバッファ（一時的なデータ置き場）作成
-#     audio_buffer = deque(maxlen=int(RATE * KEYWORD_CHUNK_SECONDS))
+    # リアルタイム音声処理の事前情報の定義
+    SCREAM_CHUNK_SECONDS = 3
+    KEYWORD_CHUNK_SECONDS = 5
+    RATE = 16000
+    CHANNELS = 1
+    FORMAT = pyaudio.paInt16
+    SCREAM_CHUNK_SIZE = int(RATE * SCREAM_CHUNK_SECONDS)
+    # 合言葉検知用に過去五秒間の音声データを保持するためのバッファ（一時的なデータ置き場）作成
+    audio_buffer = deque(maxlen=int(RATE * KEYWORD_CHUNK_SECONDS))
 
-#     p = pyaudio.PyAudio()
-#     # マイク入力から音声データを流すストリームの作成
-#     stream = p.open(
-#         format=FORMAT,
-#         channels=CHANNELS,
-#         rate=RATE,
-#         input=True,
-#         frames_per_buffer=SCREAM_CHUNK_SIZE
-#     )
+    p = pyaudio.PyAudio()
+    # マイク入力から音声データを流すストリームの作成
+    stream = p.open(
+        format=FORMAT,
+        channels=CHANNELS,
+        rate=RATE,
+        input=True,
+        frames_per_buffer=SCREAM_CHUNK_SIZE
+    )
 
-#     print("リアルタイム検知を開始します。Ctrl+Cで終了します。")
-#     print(f"合言葉検知：{'ON' if KEYWORD_DETECTION_MODE else 'OFF'}\n悲鳴検知：{'ON' if SCREAM_DETECTION_MODE else 'OFF'}")
+    print("リアルタイム検知を開始します。Ctrl+Cで終了します。")
+    print(f"合言葉検知：{'ON' if KEYWORD_DETECTION_MODE else 'OFF'}\n悲鳴検知：{'ON' if SCREAM_DETECTION_MODE else 'OFF'}")
 
-#     # メインループ
-#     try:
-#         loop_count = 0
-#         while True:
-#             data_bytes = stream.read(SCREAM_CHUNK_SIZE) # 3s分の音声データ（バイトデータ）
-#             chunk_data_int16 = np.frombuffer(data_bytes, dtype=np.int16)
+    # メインループ
+    try:
+        loop_count = 0
+        while True:
+            data_bytes = stream.read(SCREAM_CHUNK_SIZE) # 3s分の音声データ（バイトデータ）
+            chunk_data_int16 = np.frombuffer(data_bytes, dtype=np.int16)
 
-#             audio_float32 = chunk_data_int16.astype(np.float32) / 32768.0
+            audio_float32 = chunk_data_int16.astype(np.float32) / 32768.0
 
-#             audio_buffer.extend(audio_float32) # 3s分の新しいデータを5s間データを保持するdequeバッファに追加
+            audio_buffer.extend(audio_float32) # 3s分の新しいデータを5s間データを保持するdequeバッファに追加
 
-#             print(f"ループ{loop_count+1}（音声取得：{SCREAM_CHUNK_SECONDS}秒）")
+            print(f"ループ{loop_count+1}（音声取得：{SCREAM_CHUNK_SECONDS}秒）")
 
-#             # 悲鳴検知(毎3秒実行)
-#             if SCREAM_DETECTION_MODE:
-#                 is_scream, score = scream_detector.detect(audio_float32, RATE)
-#                 if is_scream:
-#                     print(f"悲鳴を検知しました（信頼度{score:.2f}）")
-#                 else:
-#                     print("悲鳴は検知されませんでした")
+            # 悲鳴検知(毎3秒実行)
+            if SCREAM_DETECTION_MODE:
+                is_scream, score = scream_detector.detect(audio_float32, RATE)
+                if is_scream:
+                    print(f"悲鳴を検知しました（信頼度{score:.2f}）")
+                else:
+                    print("悲鳴は検知されませんでした")
             
-#             # 合言葉検知(毎5秒実行)
-#             if KEYWORD_DETECTION_MODE and len(audio_buffer) == audio_buffer.maxlen:
-#                 keyword_audio_data = np.array(audio_buffer)
+            # 合言葉検知(毎5秒実行)
+            if KEYWORD_DETECTION_MODE and len(audio_buffer) == audio_buffer.maxlen:
+                keyword_audio_data = np.array(audio_buffer)
 
-#                 if keyword_detector.detect(keyword_audio_data, keyword_manager):
-#                     print("合言葉を検知しました")
-#                 else:
-#                     print("合言葉は検知されませんでした")
+                if keyword_detector.detect(keyword_audio_data, keyword_manager):
+                    print("合言葉を検知しました")
+                else:
+                    print("合言葉は検知されませんでした")
                 
-#             loop_count += 1
+            loop_count += 1
 
-#     except (KeyboardInterrupt, SystemExit):
-#         print("\nプログラムを終了する")
+    except (KeyboardInterrupt, SystemExit):
+        print("\nプログラムを終了する")
     
 #     finally:
 #         stream.stop_stream()
